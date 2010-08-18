cc829830a434e19c3d39e85f04eed2b0f5834c2e
Steven Shaw <steshaw@gmail.com>
2010-08-18 15:41:57 +1000

    make find a bit more *nixy

--- realworldhaskell/find.hs
+++ realworldhaskell/find.hs
@@ -5,21 +5,34 @@
 module Main where
 
 import System.Environment (getArgs)
-import Control.Monad (forM)
-import System.Directory (doesDirectoryExist, getDirectoryContents)
+import Control.Monad (forM, forM_, when)
+import System.Directory (doesFileExist, doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
 
 find :: FilePath -> IO [FilePath]
-find topDir = do
-  names <- getDirectoryContents topDir
-  let properNames = filter (`notElem` [".", ".."]) names
-  paths <- forM properNames $ \ name -> do
-    let path = topDir </> name
-    isDirectory <- doesDirectoryExist path
-    if isDirectory then find path else return [path]
-  return (concat paths)
+find topPath = do
+  isDir <- doesDirectoryExist topPath
+  if isDir 
+    then do
+      names <- getDirectoryContents topPath
+      let properNames = filter (`notElem` [".", ".."]) names
+      --let properNames = names -- keep "." and ".." if they appear
+      paths <- forM properNames $ \ name -> do
+        let path = topPath </> name
+        isDirectory <- doesDirectoryExist path
+        if isDirectory then find path else return [path]
+      return (topPath:(concat paths))
+    else do
+      isFile <- doesFileExist topPath
+      if isFile
+        then return [topPath]
+        else error ("'" ++ topPath ++ "': no such file or directory")
 
 main = do
+{-
   getArgs >>= \args -> 
     case args of
       [path] -> find path >>= mapM_ putStrLn
+-}
+  args <- getArgs
+  forM_ args $ \ path -> find path >>= mapM_ putStrLn
