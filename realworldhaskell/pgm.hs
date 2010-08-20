46ccef5bf6dcce6d2402738979e01cb121e44ff6
Steven Shaw <steshaw@gmail.com>
2010-08-20 12:55:54 +1000

    Handle out of range maxGrey value

--- realworldhaskell/pgm.hs
+++ realworldhaskell/pgm.hs
@@ -58,12 +58,14 @@ parseP5 s =
                           case parseNat s of
                             Nothing -> Nothing
                             Just (maxGrey, s) ->
-                              case skipSpaces s of
-                                Nothing -> Nothing
-                                Just s  ->
-                                  case parseNumBytes (width * height) s of
-                                    Nothing -> Nothing
-                                    Just (bitmap, s) -> Just (Greymap (PgmInfo width height maxGrey) bitmap, s)
+                              if maxGrey > 255 || maxGrey <= 0 then Nothing
+                              else
+                                case skipSpaces s of
+                                  Nothing -> Nothing
+                                  Just s  ->
+                                    case parseNumBytes (width * height) s of
+                                      Nothing -> Nothing
+                                      Just (bitmap, s) -> Just (Greymap (PgmInfo width height maxGrey) bitmap, s)
 
 skipHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
 skipHeader prefix s =
