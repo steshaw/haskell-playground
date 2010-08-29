b4213f4cd9cda47e1fb5cc4b2d45ddd5d72a2301
Steven Shaw <steshaw@gmail.com>
2010-08-29 15:10:00 +1000

    Desugared monads and pattern matching. Wont compile :(

---
+++ realworldhaskell/DesugarMonads.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main () where
+
+import Steshaw
+import Prelude hiding (catch)
+import Control.Exception
+
+go_1 xs =
+  return xs >>= \ (x:xs) -> return x
+
+go_2 xs = do
+  (x:xs) <- return xs
+  return x
+
+main = do
+  a <- go_1 [1..3]
+  putStrLn $ "go_1 [1..3] => " ++ show a
+  a <- go_2 [1..3]
+  putStrLn $ "go_2 [1..3] => " ++ show a
+
+  catch (do
+    a <- go_1 ([] :: [Integer])
+    putStrLn $ "go_1 [] => " ++ show a)
+    (\ (err :: PatternMatchFail) -> do putStrLn $ "Caught error: " ++ show err)
+  catch (do
+    a <- go_2 ([] :: [Integer])
+    putStrLn $ "go_2 [] => " ++ show a)
+    (\ (err :: SomeException) -> do putStrLn $ "Caught error: " ++ show err)
+
+    -- FIXME: The intent here is to show the difference between go_1 and go_2.
+    -- FIXME: i.e. in the Maybe monad, because fail is called when a pattern match fails,
+    -- FIXME: and fail for Maybe monad returns Nothing - this will be *different* from
+    -- FIXME: the go_1 case where the failed pattern match always raises an exception.
+    -- FIXME: Note that this is easy to test in ghci...
+{-
+  a <- ((go_1 [1..3]) :: Maybe Integer)
+  putStrLn $ "go_1 [1..3] => " ++ show a
+  a <- ((go_2 [1..3]) :: Maybe Integer)
+  putStrLn $ "go_2 [1..3] => " ++ show a
+-}
+
+{-
+  catch (do
+    (a::Maybe Integer) <- ((go_1 ([] :: [Integer])) :: Maybe Integer)
+    --putStrLn $ "go_1 [] => " ++ show a
+    putStrLn "ok"
+    )
+    (\ (err :: PatternMatchFail) -> do putStrLn $ "Caught error: " ++ show err)
+  catch (do
+    a <- ((go_2 ([] :: [Integer])) :: Maybe Integer)
+    --putStrLn $ "go_2 [] => " ++ show a
+    putStrLn "ok"
+    )
+    (\ (err :: SomeException) -> do putStrLn $ "Caught error: " ++ show err)
+-}
