24ba8c321d0dd4d2e9d8319f7029626316d2e050
Steven Shaw <steshaw@gmail.com>
2010-09-03 12:36:44 +1000

    Improved error reporting

--- realworldhaskell/csv.hs
+++ realworldhaskell/csv.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import Text.ParserCombinators.Parsec
+import Text.Regex
 import System.IO
+import System.Environment
 
 csvFile :: GenParser Char st [[String]]
 csvFile = line `endBy` eol
@@ -16,13 +18,13 @@ regularCell :: GenParser Char st String
 regularCell = many (noneOf [',', '\n', '\r'])
 
 quotedCell :: GenParser Char st String
-quotedCell = char '"' >> many quotedChar >>= \r -> char '"' >> return r
+quotedCell = char '"' >> many quotedChar >>= \r -> (char '"' <?> "end-double-quote") >> return r
 
 quotedChar :: GenParser Char st Char
 quotedChar = noneOf ['"'] <|> quotedDoubleQuote
 
 quotedDoubleQuote :: GenParser Char st Char
-quotedDoubleQuote = try (string (replicate 2 '"') >> return '"')
+quotedDoubleQuote = try (string (replicate 2 '"') >> return '"') <?> "quoted-double-quote"
 
 eol :: GenParser Char st ()
 eol = try (string "\r\n" >> return ())
@@ -36,6 +38,9 @@ parseCsv input = parse csvFile "(stdin)" input
 main :: IO ()
 main = do
   c <- getContents
+  progName <- getProgName
   case parseCsv c of
     Right r -> mapM_ print r
-    Left  e -> hPutStrLn stderr $ "Error parsing input:\n" ++ (show e)
+    Left  e -> hPutStrLn stderr $ progName ++ ": Error parsing input:\n" ++ (indent (show e))
+  where
+    indent s = subRegex (mkRegex "^") s "  "
