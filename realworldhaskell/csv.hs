17ab139a0cf6cade6c9642212b8fe6636667402e
Steven Shaw <steshaw@gmail.com>
2010-09-03 11:03:35 +1000

    Some golfing

--- realworldhaskell/csv.hs
+++ realworldhaskell/csv.hs
@@ -4,32 +4,18 @@ import Text.ParserCombinators.Parsec
 import System.IO
 
 csvFile :: GenParser Char st [[String]]
-csvFile = do
-  result <- many line
-  eof
-  return result
+csvFile = line `endBy` eol
 
 line :: GenParser Char st [String]
-line = do
-  result <- cells
-  eol
-  return result
-
-cells :: GenParser Char st [String]
-cells = do
-  first <- cellContent
-  next <- remainingCells
-  return $ first : next
-
-remainingCells :: GenParser Char st [String]
-remainingCells = 
-  (char ',' >> cells) <|> return []
+line = cellContent `sepBy` (char ',')
 
 cellContent :: GenParser Char st String
-cellContent = many (noneOf [',', '\n'])
+cellContent = many (noneOf [',', '\n', '\r'])
 
-eol :: GenParser Char st Char
-eol = char '\n'
+eol :: GenParser Char st ()
+eol = try (string "\r\n" >> return ())
+  <|> (string "\n" >> return ())
+  <|> (string "\r" >> return ())
 
 parseCsv :: String -> String -> Either ParseError [[String]]
 parseCsv fileName input = parse csvFile fileName input
