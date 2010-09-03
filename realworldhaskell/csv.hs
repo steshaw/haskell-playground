13a291989009fdfcd40132a99c01fd42ff5e9c96
Steven Shaw <steshaw@gmail.com>
2010-09-03 11:57:11 +1000

    Trying to parse quoted strings

--- realworldhaskell/csv.hs
+++ realworldhaskell/csv.hs
@@ -10,19 +10,27 @@ line :: GenParser Char st [String]
 line = cell `sepBy` (char ',')
 
 cell :: GenParser Char st String
-cell = many (noneOf [',', '\n', '\r'])
+cell = quotedCell <|> regularCell
+
+regularCell = many (noneOf [',', '\n', '\r'])
+
+quotedCell = char '"' >> many (noneOf ['"']) >>= \r -> char '"' >> return r
+
+quotedChar = noneOf ['"'] <|> doubleQuote
+doubleQuote = try (string (replicate 2 '"')) >> return '"'
 
 eol :: GenParser Char st ()
 eol = try (string "\r\n" >> return ())
   <|> (string "\n" >> return ())
   <|> (string "\r" >> return ())
+  <?> "end of line"
 
-parseCsv :: String -> String -> Either ParseError [[String]]
-parseCsv fileName input = parse csvFile fileName input
+parseCsv :: String -> Either ParseError [[String]]
+parseCsv input = parse csvFile "(stdin)" input
 
 main :: IO ()
 main = do
   c <- getContents
-  case parseCsv "(stdin)" c of
+  case parseCsv c of
     Right r -> mapM_ print r
     Left  e -> hPutStrLn stderr $ "Error parsing input:\n" ++ (show e)
