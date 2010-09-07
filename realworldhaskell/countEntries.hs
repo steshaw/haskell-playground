af7d33c8cead6be655b828fbcb4503815566cafa
Steven Shaw <steshaw@gmail.com>
2010-09-07 21:42:27 +1000

    introduce newtype

--- realworldhaskell/countEntries.hs
+++ realworldhaskell/countEntries.hs
@@ -1,11 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
 import Control.Monad (forM_, liftM, forM_)
-import Control.Monad.Trans (lift, liftIO)
-import Control.Monad.Reader (ask, ReaderT, runReaderT)
-import Control.Monad.State (get, put, StateT, runStateT)
+import Control.Monad.Trans (lift, liftIO, MonadIO)
+import Control.Monad.Reader (ask, runReaderT, ReaderT, MonadReader)
+import Control.Monad.State (get, put, StateT, runStateT, StateT, MonadState)
 import Control.Monad.Writer (tell, WriterT, execWriterT)
 import Control.Applicative ((<$>), (<*>))
 import Control.Arrow (first, second)
@@ -20,13 +21,16 @@ data AppState = AppState {
 } deriving (Show)
 
 type App = ReaderT AppConfig (StateT AppState IO)
+newtype NewApp a = NewApp {
+  getMyApp :: App a
+} deriving (Monad, MonadIO, MonadReader AppConfig, MonadState AppState)
 
 listDirectory :: FilePath -> IO [String]
 listDirectory = liftM (filter notDots) . getDirectoryContents
   --where notDots path = path /= "." && path /= ".."
   where notDots = (&&) <$> (/= ".") <*> (/= "..") -- alternative (perhaps evil) implementation
 
-countEntries :: FilePath -> WriterT [(FilePath, Int)] App ()
+countEntries :: FilePath -> WriterT [(FilePath, Int)] NewApp ()
 countEntries path = do
   cfg <- ask
   contents <- liftIO . listDirectory $ path
@@ -45,8 +49,9 @@ countEntries path = do
 
 countEntries' path = execWriterT (countEntries path)
 
-countEntries'' maxDepth path = 
-  runStateT (runReaderT (countEntries' path) AppConfig {cfgMaxDepth = maxDepth}) AppState {stDepth = 0, stMaxDepth = 0}
+countEntries'' maxDepth path =
+  runStateT (runReaderT (getMyApp (countEntries' path)) AppConfig {cfgMaxDepth = maxDepth})
+            AppState {stDepth = 0, stMaxDepth = 0}
 
 eg1 :: FilePath -> IO [(String, Int)]
 eg1 path = liftM (map (first ("dir: " ++))) $ liftM (take 2 . fst) (countEntries'' 1 path)
