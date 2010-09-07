a035fea9b3c7af84d9b53f6f42e4bcd6430676db
Steven Shaw <steshaw@gmail.com>
2010-09-07 18:18:30 +1000

    monad transformers chapter - intial

---
+++ realworldhaskell/CountEntries.hs
@@ -0,0 +1,21 @@
+module CountEntries where
+
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath ((</>))
+import Control.Monad (forM, liftM)
+
+listDirectory :: FilePath -> IO [String]
+listDirectory = liftM (filter notDots) . getDirectoryContents
+  where notDots path = path /= "." && path /= ".."
+  --where notDots path = (&&) <$> (/= ".") <*> (/= "..")
+
+countEntries :: FilePath -> IO [(FilePath, Int)]
+countEntries path = do
+  contents <- listDirectory path
+  rest <- forM contents $ \ name -> do
+    let newName = path </> name
+    isDir <- doesDirectoryExist newName
+    if isDir 
+      then countEntries newName
+      else return []
+  return $ (path, length contents) : concat rest
