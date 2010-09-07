c42564ac83aa8b111181d301a3c29cb7ac9fd957
Steven Shaw <steshaw@gmail.com>
2010-09-07 20:04:45 +1000

    Introduce WriterT over IO

--- realworldhaskell/CountEntries.hs
+++ realworldhaskell/CountEntries.hs
@@ -2,7 +2,9 @@ module CountEntries where
 
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
-import Control.Monad (forM, liftM)
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Writer
 import Control.Applicative
 
 listDirectory :: FilePath -> IO [String]
@@ -10,13 +12,14 @@ listDirectory = liftM (filter notDots) . getDirectoryContents
   --where notDots path = path /= "." && path /= ".."
   where notDots = (&&) <$> (/= ".") <*> (/= "..") -- alternative (perhaps evil) implementation
 
-countEntries :: FilePath -> IO [(FilePath, Int)]
+countEntries :: FilePath -> WriterT [(FilePath, Int)] IO ()
 countEntries path = do
-  contents <- listDirectory path
-  rest <- forM contents $ \ name -> do
-    let newName = path </> name
-    isDir <- doesDirectoryExist newName
+  contents <- liftIO . listDirectory $ path
+  tell [(path, length contents)]
+  forM_ contents $ \ name -> do
+    let newPath = path </> name
+    isDir <- liftIO . doesDirectoryExist $ newPath
     if isDir 
-      then countEntries newName
-      else return []
-  return $ (path, length contents) : concat rest
+      then countEntries newPath
+      else return ()
+
