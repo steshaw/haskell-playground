6e10cdd2ddd686938ca13e387469a3af77e4e2fc
Steven Shaw <steshaw@gmail.com>
2010-09-11 16:42:49 +1000

    Tiny parsing framework from RWH ch19

---
+++ realworldhaskell/BabyParse.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module BabyParse where
+
+import Control.Monad.Error
+import Control.Monad.State
+import qualified Data.ByteString.Char8 as B
+import Data.Char
+
+data ParseError 
+  = NumericOverflow
+  | EndOfInput
+  | Chatty String
+    deriving (Eq, Ord, Show)
+
+instance Error ParseError where
+  noMsg = Chatty "oh noes!"
+  strMsg = Chatty
+
+newtype Parser a = Parser {
+  getParser :: ErrorT ParseError (State B.ByteString) a
+} deriving (Monad, MonadError ParseError)
+
+liftP :: State B.ByteString a -> Parser a
+liftP m = Parser (lift m)
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = do
+  s <- liftP get
+  case B.uncons s of
+    Nothing -> throwError EndOfInput
+    Just (c, s)
+      | p c       -> liftP (put s) >> return c
+      | otherwise -> throwError (Chatty "satisfy failed :(")
+
+optional :: Parser a -> Parser (Maybe a)
+optional p = (liftM Just p) `catchError` \_ -> return Nothing
+
+letter = satisfy isLetter
+
+optionalLetter = optional letter
+
+eg1 = runState (runErrorT (getParser $ letter)) (B.pack "asdf")
+eg2 = runState (runErrorT (getParser $ letter)) (B.pack "2.3 + 5.1")
+eg3 = runState (runErrorT (getParser $ optionalLetter)) (B.pack "asdf")
+eg4 = runState (runErrorT (getParser $ optionalLetter)) (B.pack "2.3 + 5.1")
