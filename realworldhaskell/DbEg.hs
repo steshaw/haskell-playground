ed3a2c3d5c2121fd41666049fcef246d25cbb404
Steven Shaw <steshaw@gmail.com>
2010-09-11 13:12:14 +1000

    Refactor withConnection

--- realworldhaskell/DbEg.hs
+++ realworldhaskell/DbEg.hs
@@ -2,36 +2,34 @@ module DbEg where
 
 import Database.HDBC
 import Database.HDBC.Sqlite3
+import Control.Monad (when)
 
 connect = connectSqlite3 "test1.db"
 
-populate = do
+withConnection f = do
   c <- connect
-  tables <- getTables c
-  if notElem "descs" tables
-    then do
+  result <- f c
+  disconnect c
+  return result
+
+populate =
+  withConnection $ \c -> do
+    tables <- getTables c
+    when (notElem "descs" tables) $ do
       run c "create table descs (id integer not null, desc varchar(200) not null)" []
       insert <- prepare c "insert into descs (id, desc) values (?, ?)"
-      mapM (execute insert) [
-        [toSql (1::Integer), toSql "one"],
-        [toSql (2::Integer), toSql "two"],
-        [toSql (3::Integer), toSql "three"],
-        [toSql (4::Integer), toSql "four"],
-        [toSql (5::Integer), toSql "five"],
-        [toSql (6::Integer), toSql "six"]
-       ]
+      mapM (execute insert)
+        [ [toSql (1::Integer), toSql "one"]
+        , [toSql (2::Integer), toSql "two"]
+        , [toSql (3::Integer), toSql "three"]
+        , [toSql (4::Integer), toSql "four"]
+        , [toSql (5::Integer), toSql "five"]
+        , [toSql (6::Integer), toSql "six"]
+        ]
       commit c
-    else return ()
-  disconnect c
 
-search s = do
-  c <- connect
-  result <- quickQuery' c "select desc from descs where desc like ?" [s]
-  disconnect c
-  return result
+search s = withConnection $ \c ->
+  quickQuery' c "select desc from descs where desc like ?" [s]
 
-allDesc = do
-  c <- connect
-  result <- quickQuery' c "select desc from descs" []
-  disconnect c
-  return result
+allDesc = withConnection $ \c ->
+  quickQuery' c "select desc from descs" []
