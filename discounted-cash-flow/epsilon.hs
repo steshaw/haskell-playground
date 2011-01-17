013c1ef502eb72cec4532372f280c85318caeaa2
Steven Shaw <steshaw@gmail.com>
2011-01-17 10:27:32 +1000

    Tidy up epsilon

--- discounted-cash-flow/epsilon.hs
+++ discounted-cash-flow/epsilon.hs
@@ -1,8 +1,9 @@
-import Steshaw
+import Steshaw hiding (epsilon)
+
 {-
 main = print . last . takeWhile (/= 1) . map (+1) . iterate (/2) $ 1
 -}
 
 epsilon = 1 $> iterate (/2) $> map (+1) $> takeWhile (/= 1) $> last $> (-) 1
 
-main = print fred
+main = print epsilon
