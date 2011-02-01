1f6c3af29f2aae678d9c4a6f0a35187baa31b31e
Steven Shaw <steshaw@gmail.com>
2011-02-01 10:35:01 +1000

    Move % to Steshaw

--- discounted-cash-flow/Steshaw.hs
+++ discounted-cash-flow/Steshaw.hs
@@ -9,6 +9,7 @@ module Steshaw
   ,d
   ,todo
   ,epsilon
+  ,(%)
 ) where
 
 import Prelude hiding (length)
@@ -48,6 +49,8 @@ d = foldl (\ acc i -> acc * 1000 + i) 0
 todo functionName = error $ "Implement " ++ functionName
 
 epsilon = 1 $> iterate (/2) $> map (+1) $> takeWhile (/= 1) $> last $> (-) 1
+
+(%) n = n / 100.0
 
 {-
 I don't know how established it is, but ($>) is a common name for flip($). Common, as in, I've seen three or so different people use it. IIRC, the F# name is (|>) but triangles are far too nice of a name and are often used for other things (e.g., cons and snoc). For what it's worth, this is the T combinator--- in case thrushes give you any ideas for names. Personally I'm more likely to use T as a prefix combinator rather than as infix, and for that the ($ _) section is good enough for me.
