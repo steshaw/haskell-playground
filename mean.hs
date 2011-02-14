96defb54a50933ac8d84c0f1ce9e8a45f38580eb
Steven Shaw <steven@steshaw.org>
2011-02-15 02:24:04 +1000

    Fix with seq

---
+++ mean.hs
@@ -0,0 +1,13 @@
+
+import Data.List (foldl')
+
+mean :: [Double] -> Double
+mean xs = s / fromIntegral l
+ where
+  (s, l) = foldl' step (0, 0) xs
+  step (s, l) a = 
+    let s' = s + a
+        l' = l + 1
+    in seq s' $ seq l' $ (s', l')
+
+main = print $ mean [1..1e8] -- will cause memory alloation failure
