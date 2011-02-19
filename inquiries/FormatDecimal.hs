c63ecfc31442f3b9f48b7b36a0e20e6ace4d625c
Steven Shaw <steven@steshaw.org>
2011-02-19 17:42:01 +1000

    More readable by pointy implemention of formatDecimal

--- inquiries/FormatDecimal.hs
+++ inquiries/FormatDecimal.hs
@@ -4,17 +4,35 @@
 module FormatDecimal (formatDecimal) where
 
 --import Data.Graph.Inductive.Query.Monad (mapFst)
-import List
+-- This isn't a standard GHC import so implemented below instead.
+-- XXX: Looks Arrows 'cause we're pointing at the first element of the tuple.
+
+import Data.List (unfoldr, intersperse)
 import Text.Printf
 
 ($>) = flip ($)
 
 mapFst :: (a -> b) -> (a, c) -> (b, c)
-mapFst f (a,b) = ((f a), b)
+mapFst f (a, b) = (,) (f a) b
 
 formatDecimal decimalPlaces d
-    | d < 0.0   = "-" ++ (formatPositiveDecimal (-d))
-    | otherwise = formatPositiveDecimal d
-    where formatPositiveDecimal = uncurry (++) . mapFst addCommas . span (/= '.') . printf ("%0." ++ decimalPlaces $> show ++ "f")
-          addCommas = reverse . concat . intersperse "," . unfoldr splitIntoBlocksOfThree . reverse
-          splitIntoBlocksOfThree l = case splitAt 3 l of ([], _) -> Nothing; p-> Just p
+  | d < 0.0   = "-" ++ (formatPositiveDecimal (-d))
+  | otherwise = formatPositiveDecimal d
+  where
+    -- Using ($>) makes the following easier to follow (for me at least).
+    -- However, it does have the downside of making it more pointy (that is, it is no longer "point free" or pointless).
+    formatPositiveDecimal d =
+      printf ("%0." ++ (decimalPlaces $> show) ++ "f") d
+        $> span (/= '.')
+        $> mapFst addCommas
+        $> uncurry (++)
+    addCommas l =
+      reverse l
+        $> unfoldr groupBy3
+        $> intersperse ","
+        $> concat
+        $> reverse
+    groupBy3 l =
+      case splitAt 3 l of 
+        ("", "") -> Nothing
+        p       -> Just p
