d502b318e5f9e2bbcf3d817858e40125ad86d7d1
Steven Shaw <steven@steshaw.org>
2011-03-27 22:00:53 +1000

    GADT example

---
+++ termGadt.hs
@@ -0,0 +1,18 @@
+--
+-- From http://www.haskell.org/ghc/docs/latest/html/users_guide/data-type-extensions.html#gadt
+--
+
+{-# LANGUAGE GADTs #-}
+
+data Term a where
+      Lit    { val  :: Int }      :: Term Int
+      Succ   { num  :: Term Int } :: Term Int
+      Pred   { num  :: Term Int } :: Term Int
+      IsZero { arg  :: Term Int } :: Term Bool              
+      Pair   { arg1 :: Term a
+             , arg2 :: Term b
+             }                    :: Term (a,b)
+      If     { cnd  :: Term Bool
+             , tru  :: Term a
+             , fls  :: Term a
+             }                    :: Term a
