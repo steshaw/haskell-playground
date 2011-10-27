2bd7a76be55730c70f593fa80663f9048527482b
Steven Shaw <steven@steshaw.org>
2011-10-27 16:05:08 +1000

    Example of run length encoding in Haskell

---
+++ runLengthEncoding.hs
@@ -0,0 +1,18 @@
+--
+-- From http://www.scribd.com/doc/48773692/Ignite-SV-Haskell-is-Better
+--
+
+runLengthEncode :: Eq a => [a] -> [(a, Int)]
+runLengthEncode xs =
+  foldr nextGroup [] xs
+    where
+      nextGroup x []        = [(x, 1)]
+      nextGroup x gl@((g, n):gs)
+        | x == g            = (g, n+1):gs
+        | otherwise         = (x, 1):gl
+
+main = 
+  let 
+    foo = "aaaaaaaaaabbbbbbbbbccccccccccD"
+    encoded = runLengthEncode foo
+  in  putStrLn (show encoded)
