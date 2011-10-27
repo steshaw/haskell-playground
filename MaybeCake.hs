876842dab8d1c13584188faa71d70aa4be4a8d3b
Steven Shaw <steven@steshaw.org>
2011-10-27 13:14:33 +1000

    Add the description

--- MaybeCake.hs
+++ MaybeCake.hs
@@ -1,3 +1,7 @@
+--
+-- From http://www.techtangents.com/the-maybe-cake-puzzle/
+--
+
 module MaybeCake where
 
 import Data.Maybe
@@ -12,10 +16,10 @@ data Chook = Chook {
   layEgg :: Maybe Egg
 }
 
-data Egg = Egg 
+data Egg = Egg
   deriving Show
 
-data Chocolate = Chocolate 
+data Chocolate = Chocolate
   deriving Show
 
 data Cocoa = Cocoa
@@ -39,14 +43,33 @@ data Bakery = Bakery {
   bakeryCake :: Maybe Cake
 }
 
-data Cake = 
+data Cake =
     MudCake Egg Chocolate Flour
-  | FlourlessCake Egg Chocolate Cocoa 
-  | BakeryCake 
-  deriving Show 
-
+  | FlourlessCake Egg Chocolate Cocoa
+  | BakeryCake
+  deriving Show
 
 -- define this
+{-
+I’d like a cake.
+
+Ideally, I’d like to bake it myself, but if I don’t have the ingredients, I’ll buy one from the 
+bakery (if they have one).
+
+I can make 2 types of cake:
+
+My favourite is a mud cake (egg, chocolate, flour).
+I can also make a flourless cake (egg, chocolate, cocoa).
+There might be some chocolate in the fridge or the pantry.
+There might be some flour in the pantry.
+There might be some cocoa in the pantry.
+
+There might be an egg in the fridge, but I’d prefer a fresh one – maybe my chook laid one?
+
+But I think I saw a taipan around the chicken coup yesterday – hopefully he didn’t get my chook!
+
+So, do I get a cake and, if so, which one?
+-}
 bakeMeACake :: Coup -> Fridge -> Pantry -> Bakery -> Maybe Cake
-bakeMeACake coup fridge pantry bakery = 
+bakeMeACake coup fridge pantry bakery =
   undefined
