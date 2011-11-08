24fb7e2aabe91c694f8121e9b6768c126d0234d0
Steven Shaw <steven@steshaw.org>
2011-11-08 17:21:35 +1000

    Emps, depts, countries, currencies example

--- hello.hs
+++ hello.hs
@@ -19,3 +19,23 @@ recChar :: Char -> Parser ()
 recChar char = \s -> case s of
   (first : rest) | first == char -> [((), rest)]
   otherwise -> []
+
+data Employee
+data Department
+data Country
+data Currency
+
+employeesDepartment :: Employee -> Maybe Department
+employeesDepartment = error "todo"
+
+departmentsCountry :: Department -> Maybe Country
+departmentsCountry = error "todo"
+
+countriesCurrency :: Country -> Maybe Currency
+countriesCurrency = error "todo"
+
+employeeCurrency :: Employee -> Maybe Currency
+employeeCurrency emp =
+  employeesDepartment emp >>= \dept ->
+  departmentsCountry dept >>= \country ->
+  countriesCurrency country
