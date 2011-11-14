11d47bdb3b930ec3ef0243d258dd9c9290a9876e
Steven Shaw <steven@steshaw.org>
2011-11-14 16:02:36 +1000

    Implement positions

--- programming-in-haskell/lecture5.hs
+++ programming-in-haskell/lecture5.hs
@@ -23,3 +23,29 @@ adjPairWith f xs = zipWith f xs (tail xs)
 
 sorted1 :: Ord a => [a] -> Bool
 sorted1 xs = and $ adjPairWith (<) xs
+
+-- recursive solution
+positions :: Eq a => a -> [a] -> [Int]
+positions a xs = iter 0 xs
+  where
+    iter n []              = []
+    iter n (x:xs) | x == a = n : iter (n+1) xs
+    iter n (x:xs) | True   = iter (n+1) xs
+
+-- foldr solution - argh but counts the positions from the right!
+positions1 :: Eq a => a -> [a] -> [Int]
+positions1 a xs = snd $ foldr f (0, []) xs
+  where
+    f e (i, xs) | e == a = (i+1, i : xs)
+    f e (i, xs) | True   = (i+1, xs)
+
+zipWithIndex :: [a] -> [(a, Integer)]
+zipWithIndex xs = zip xs [0..]
+
+-- using zipWithIndex with comprehension
+positions2 :: Eq a => a -> [a] -> [Integer]
+positions2 n xs = [ i | (x, i) <- zipWithIndex xs, x == n]
+
+-- using zipWithIndex and map/filter
+positions3 :: Eq a => a -> [a] -> [Integer]
+positions3 n xs = map snd $ filter (\(x, i) -> x == n) $ zipWithIndex xs
