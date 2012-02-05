30762ed59dfa7006b3d09e16cca3bfd65d1a7333
Steven Shaw <steven@steshaw.org>
2012-02-05 16:48:54 +1000

    Imp now accepts filename as arg

--- imperative/Main.hs
+++ imperative/Main.hs
@@ -1,3 +1,7 @@
+--
+-- Hacked by Steven Shaw to accept filename as arg.
+--
+
 -- Main module for Imp parser and interpreter.
 -- Developed for use in COMP3610
 -- Clem Baker-Finch
@@ -7,24 +11,12 @@ module Main (main) where
 import IO
 import Parser
 import Interpreter
-
--- The usual general file opening function.
-
-getAndOpenFile :: String -> IOMode -> IO Handle
-getAndOpenFile prompt mode =
-    do putStr prompt
-       name <- getLine
-       catch (openFile name mode)
-	     (\_ -> do putStrLn ("Cannot open " ++ name ++ "\n")
-                       getAndOpenFile prompt mode)
-
--- Main function: get the source Imp file, parse it, interpret it and
--- print the resulting state to stdout.
+import System (getArgs)
 
 main :: IO()
 main = do
-       sourceFile <- getAndOpenFile "Path of Imp source file: " ReadMode
-       imp <- hGetContents sourceFile
-       let result = eC (parseProg imp) arid
-       putStr (show result)
-
+  [filename] <- getArgs
+  file <- openFile filename ReadMode
+  input <- hGetContents file
+  let result = eC (parseProg input) arid
+  putStr (show result)
