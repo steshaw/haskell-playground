8f9682912400238a5e6193d4334dd8f94b2d0b42
Steven Shaw <steven@steshaw.org>
2012-02-09 22:55:30 +1000

    Partially implemented arithexcept. Interpreter remains

---
+++ towards-modular-compilers-for-effects/arithexcept1.hs
@@ -0,0 +1,57 @@
+--
+-- From slides http://www.cs.nott.ac.uk/~led/talks/led_modularcompilers_slides.pdf
+--
+
+import Control.Monad
+
+data Expr
+  = Val Int
+  | Add Expr Expr
+  | Throw
+  | Catch Expr Expr
+  deriving (Show)
+
+eval :: Expr -> Maybe Int
+eval (Val n)     = return n
+eval (Add x y)   = do n <- eval x
+                      m <- eval y
+                      return (n + m)
+eval Throw       = mzero
+eval (Catch x h) = eval x `mplus` eval h
+
+type Code = [Op]
+data Op
+  = PUSH Int
+  | ADD
+  | THROW
+  | MARK Code
+  | UNMARK
+  deriving (Show)
+
+comp :: Expr -> Code
+comp (Val n)     = [PUSH n]
+comp (Add x y)   = comp x ++ comp y ++ [ADD]
+comp Throw       = [THROW]
+comp (Catch x h) = [MARK (comp h)] ++ comp x ++ [UNMARK]
+
+exec :: Code -> Int
+exec = exec' []
+
+type Stack = [Int]
+
+exec' :: Stack -> Code -> Int
+exec' [result] []                = result
+exec' stack (PUSH n : cs)        = exec' (n : stack) cs
+exec' (a : b : stack) (ADD : cs) = exec' (a + b : stack) cs
+
+main = do
+  let e = Add (Val 1) (Add (Val 2) (Val 3))
+  putStr "expr: "
+  print e
+  putStr "eval: "
+  print $ eval e
+  putStr "comp: "
+  let code = comp e
+  print $ code
+  putStr "exec: "
+  print $ exec code
