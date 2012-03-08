e898da31f6852e2c879e0bb16ae56e8fb231e6c3
Steven Shaw <steven@steshaw.org>
2012-03-08 19:26:57 +1000

    "looping" examples in Haskell

---
+++ loops.hs
@@ -0,0 +1,34 @@
+--
+-- From http://www.xoltar.org/old_site/2003//sep/09/haskellLoops.html
+--
+
+import Data.IORef
+
+-- Define 'foreach'
+foreach = flip mapM_
+
+-- Define 'while'
+while test action = do
+  val <- test
+  if val then do {action;while test action}
+         else return ()
+
+-- Some helpers for use with 'while':
+incr ref = modifyIORef ref (+1)
+test ref f = do { val <- readIORef ref; return (f val) }
+
+-- Exercise them. Equivalent Python code:
+-- for x in range(1,11): 
+--     print x
+-- i = 0
+-- while i < 5:
+--     print "Still running"
+--     i += 1
+
+main = do
+  foreach [1..10] print
+  ref <- newIORef 0
+  while (test ref (< 5))
+        (do 
+           print "Still running!"
+           incr ref)
