26d785cef6dba66d09b57ad677c242ed1e1e5378
Steven Shaw <steven@steshaw.org>
2012-04-02 21:21:51 +1000

    Implement lens product morphism i.e. (***)

--- asymmetric-lenses/Person.hs
+++ asymmetric-lenses/Person.hs
@@ -12,10 +12,20 @@ data Person = Person
   }
   deriving (Show)
 
-person = Person 
+person = Person
   { age = 9
   , address = Address
     { street = "Edward Street"
+    , state = "QLD"
+    }
+  }
+
+person1 = person
+
+person2 = Person
+  { age = 23
+  , address = Address
+    { street = "Charlotte Street"
     , state = "QLD"
     }
   }
--- asymmetric-lenses/asymmetricLenses.hs
+++ asymmetric-lenses/asymmetricLenses.hs
@@ -81,3 +81,18 @@ newPerson = set personStreetL (person, "Elizabeth Street")
 
 foldLens :: [Lens a a] -> Lens a a
 foldLens xs = foldr composeL idL xs
+
+--
+-- from 5.1 Lens Product Morphism
+--
+(***) :: Lens r1 f1 -> Lens r2 f2 -> Lens (r1, r2) (f1, f2)
+(***) l1 l2 = Lens
+  { get = \r -> (get l1 (fst r), get l2 (snd r))
+  , set = \(r, f) -> (set l1 ((fst r), (fst f))
+                     ,set l2 ((snd r), (snd f))
+                     )
+  }
+
+eg1 = get (personStreetL *** ageL) (person1, person2)
+
+eg2 = set (personStreetL *** ageL) ((person1, person2), ("Ann Street", 33))
