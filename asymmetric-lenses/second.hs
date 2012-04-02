e55a4eee634abf8ad837b118c1b7af24977a9619
Steven Shaw <steven@steshaw.org>
2012-04-02 19:08:24 +1000

    CoState map coFlatMap and coFlatten

--- asymmetric-lenses/second.hs
+++ asymmetric-lenses/second.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 --
 -- Second part of the paper.
 --
@@ -6,13 +8,8 @@
 -- 4. Representing an Asymmetric Lens in Scala
 --
 
-data CoState field record = CoState
-  { get :: field
-  , set :: field -> record
-  }
-
 -- A fused lens.
-data FLens record field = FLens
+newtype FLens record field = FLens
   { apply :: record -> CoState field record
   }
 
@@ -29,3 +26,27 @@ flatMapState :: State s a -> (a -> State s b) -> State s b
 flatMapState sm f = State
   { run = \s -> let (a, t) = (run sm) s in run (f a) t
   }
+
+data CoState field record = CoState
+  { get :: field
+  , set :: field -> record
+  }
+
+-- mapCoState :: (a -> b) -> CoState s a -> CoState s b
+mapCoState :: (r -> s) -> CoState f r -> CoState f s
+mapCoState f csm = CoState
+  { get = get csm
+  , set = f . (set csm)
+  }
+
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- extend :: (w a -> b) -> w a -> w b
+coFlatMap :: CoState f r -> (CoState f r -> s) -> CoState f s
+coFlatMap csm f = CoState 
+  { get = get csm
+  , set = \k -> f $ CoState {get = k, set = (set csm)}
+  }
+
+-- join :: Monad m => m (m a) -> m a
+coFlatten :: forall f r. (CoState f) r -> (CoState f) ((CoState f) r)
+coFlatten csm = coFlatMap csm id
