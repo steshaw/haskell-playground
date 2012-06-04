17d3614b107dfe05ccd0bec52f421b65df7d108f
Steven Shaw <steven@steshaw.org>
2012-06-04 15:59:05 +1000

    Add nat plus in terms of foldNat

--- foldNat.hs
+++ foldNat.hs
@@ -6,7 +6,15 @@ import Prelude hiding (succ)
 data Nat
   = Zero
   | Succ Nat
+  deriving (Eq, Read, Show)
 
 foldNat :: forall a. a -> (a -> a) -> Nat -> a
 foldNat zero _ (Zero)      = zero
 foldNat zero succ (Succ n) = succ (foldNat zero succ n)
+
+plus :: Nat -> Nat -> Nat
+plus Zero n = n
+plus (Succ k) n = Succ (plus k n)
+
+plus' :: Nat -> Nat -> Nat
+plus' m n = foldNat n (\rec -> Succ rec) m
