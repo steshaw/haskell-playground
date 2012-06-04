34fa989a3fc21080375c22623f05d9193eb66450
Steven Shaw <steven@steshaw.org>
2012-06-04 14:06:48 +1000

    Get the evaluator working with simply-typed lambda calculus

--- SimplyEasier/typed.hs
+++ SimplyEasier/typed.hs
@@ -17,53 +17,52 @@ data Expr
   | Lam Sym Type Expr
   deriving (Eq, Read, Show)
 
-{-
 whnf :: Expr -> Expr
 whnf ee = spine ee []
   where spine (App f a) as = spine f (a:as)
-        spine (Lam s e) (a:as) = spine (subst s a e) as
+        spine (Lam s t e) (a:as) = spine (subst s t a e) as
         spine f as = foldl App f as
 
 freeVars :: Expr -> [Sym]
 freeVars (Var s) = [s]
 freeVars (App f a) = freeVars f `union` freeVars a
-freeVars (Lam i e) = freeVars e \\ [i]
+freeVars (Lam i t e) = freeVars e \\ [i]
 
 --
 -- Beta reduction
 --
-subst :: Sym -> Expr -> Expr -> Expr
-subst v x b = sub b
+subst :: Sym -> Type -> Expr -> Expr -> Expr
+subst v t x b = sub b
   where sub e@(Var i) = if i == v then x else e
         sub (App f a) = App (sub f) (sub a)
-        sub (Lam i e) =
+        sub l@(Lam i t e) =
           if v == i then
-            Lam i e
+            l
           else if i `elem` fvx then
             let i' = cloneSym e i
-                e' = substVar i i' e
-            in  Lam i' (sub e')
+                e' = substVar i i' t e
+            in  Lam i' t (sub e')
           else
-            Lam i (sub e)
+            Lam i t (sub e)
         fvx = freeVars x
         cloneSym e i = loop i
           where loop i' = if i' `elem` vars then loop (i ++ "'") else i'
                 vars = fvx ++ freeVars e
 
-substVar :: Sym -> Sym -> Expr -> Expr
-substVar s s' e = subst s (Var s') e
+substVar :: Sym -> Sym -> Type -> Expr -> Expr
+substVar s s' t e = subst s t (Var s') e
 
 alphaEq :: Expr -> Expr -> Bool
 alphaEq (Var v)   (Var v')    = v == v'
 alphaEq (App f a) (App f' a') = alphaEq f f' && alphaEq a a'
-alphaEq (Lam s e) (Lam s' e') = alphaEq e (substVar s' s e')
+alphaEq (Lam s t e) (Lam s' t' e') = alphaEq e (substVar s' s t e')
 alphaEq _ _ = False
 
 nf :: Expr -> Expr
 nf ee = spine ee []
   where spine (App f a) as = spine f (a:as)
-        spine (Lam s e) [] = Lam s (nf e)
-        spine (Lam s e) (a:as) = spine (subst s a e) as
+        spine (Lam s t e) [] = Lam s t (nf e)
+        spine (Lam s t e) (a:as) = spine (subst s t a e) as
         spine f as = app f as
         app f as = foldl App f (map nf as)
 
@@ -76,8 +75,6 @@ betaEq e1 e2 = alphaEq (nf e1) (nf e2)
   two ≡ λs.λz.s (s z)
   three ≡ λs.λz.s (s (s z))
   plus ≡ λm.λn.λs.λz.m s (n s z)
--}
-
 -}
 [z,s,m,n] = map (Var . (:[])) "zsmn"
 app2 f x y = App (App f x) y
