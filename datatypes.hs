fc55d39e3dbde3b217dcc5e36b4e56096af91912
Steven Shaw <steven@steshaw.org>
2013-01-24 14:56:40 +1000

    Triples and booleans

--- datatypes.hs
+++ datatypes.hs
@@ -30,11 +30,33 @@ snd' = \a b -> b
 -- Tuples --
 ------------
 
+type Triple a b c = (a, b, c)
+
+-- Construction
 triple :: a -> b -> c -> (a -> b -> c -> t) -> t
 triple = \a b c f -> f a b c
+
+-- Access
 
 showTriple t = t (\a b c -> "(" ++ show a ++ ", " ++ show b ++ ", " ++ show c ++ ")")
 
 first t = t (\a b c -> a)
 second t = t (\a b c -> b)
 third t = t (\a b c -> c)
+
+-------------
+-- Boolean --
+-------------
+
+-- Construction: True or False
+-- booleans have two different constructors.
+true = \f t -> t
+false = \f t -> f
+
+--true f t = t
+--false f t = f
+
+-- Destructuring.
+-- Encoding of 'if' then else
+
+if_ b whenTrue whenFalse = b whenFalse whenTrue
