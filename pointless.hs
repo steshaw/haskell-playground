1064c6a2bc7136108c152b14240e8e2cf4b6df1e
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:20:51 +1000

    Could be progress

--- pointless.hs
+++ pointless.hs
@@ -4,8 +4,8 @@ summer = (+)
 lengther = (+) . const 1
 myapp = uncurry (***)
 dup f a = f a a
-bit xs = foldr (myapp . (summer *** lengther)) (0.0, 0.0) (dup zip xs)
-average = bit >>> uncurry (/)
+bit = foldr (myapp . (summer *** lengther)) (0.0, 0.0)
+average xs = uncurry (/) $ bit (dup zip xs)
 
 xs = [2.0, 5.0, 7.0]
 
