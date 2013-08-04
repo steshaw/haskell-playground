c268e049ae2fbe7b02a7dc68c9f8f45ae58f804a
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:14:04 +1000

    More pointless

--- pointless.hs
+++ pointless.hs
@@ -4,7 +4,7 @@ summer = (+)
 lengther = (+) . const 1
 myapp = uncurry (***)
 dup f a = f a a
-bit xs = foldr (\a -> myapp ((summer *** lengther) a)) (0.0, 0.0) (dup zip xs)
+bit xs = foldr (myapp . (summer *** lengther)) (0.0, 0.0) (dup zip xs)
 average = bit >>> uncurry (/)
 
 xs = [2.0, 5.0, 7.0]
