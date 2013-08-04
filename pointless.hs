0e403f5e7f1ff38ec58c277ddf8da9896ad5f9d6
Steven Shaw <steshaw@gmail.com>
2013-08-04 21:57:46 +1000

    Bit less pointy

--- pointless.hs
+++ pointless.hs
@@ -5,6 +5,8 @@ lengther = (+) . const 1
 myapp (fa, fb) (a, b) = (fa a, fb b)
 dup f a = f a a
 bit xs = foldr (\a -> myapp ((summer *** lengther) a)) (0.0, 0.0) (dup zip xs)
-average xs = uncurry (/) (bit xs)
+average = bit >>> uncurry (/)
 
 xs = [2.0, 5.0, 7.0]
+
+result = average xs
