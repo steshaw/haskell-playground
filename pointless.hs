16bcb42f6f56b97c1c6ff711ce438d75ee678f30
Steven Shaw <steshaw@gmail.com>
2013-08-04 23:37:48 +1000

    Switch to foldl

--- pointless.hs
+++ pointless.hs
@@ -2,7 +2,7 @@ import Control.Arrow
 import Control.Monad
 
 average :: [Double] -> Double
-average = uncurry (/) . foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0) . join zip
+average = uncurry (/) . foldl (uncurry (***) . ((+) *** flip ((+) . const 1))) (0.0, 0) . join zip
 
 xs :: [Double]
 xs = [2.0, 5.0, 7.0]
