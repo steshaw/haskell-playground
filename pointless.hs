b1fa6308185f92b14d9b4d3825d76e52d11c4253
Steven Shaw <steshaw@gmail.com>
2013-08-04 21:55:26 +1000

    More pointy

--- pointless.hs
+++ pointless.hs
@@ -3,4 +3,8 @@ import Control.Arrow
 summer = (+)
 lengther = (+) . const 1
 myapp (fa, fb) (a, b) = (fa a, fb b)
-average xs = uncurry (/) (foldr (\a -> myapp ((summer *** lengther) a)) (0.0, 0.0) (zip xs xs))
+dup f a = f a a
+bit xs = foldr (\a -> myapp ((summer *** lengther) a)) (0.0, 0.0) (dup zip xs)
+average xs = uncurry (/) (bit xs)
+
+xs = [2.0, 5.0, 7.0]
