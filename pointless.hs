eecb651f28d69ed617971fbeff29f4b774cd674f
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:26:11 +1000

    Inline bit

--- pointless.hs
+++ pointless.hs
@@ -1,8 +1,7 @@
 import Control.Arrow
 
 dup f a = f a a
-bit = foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0.0)
-average = uncurry (/) . bit . dup zip
+average = uncurry (/) . foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0.0) . dup zip
 
 xs = [2.0, 5.0, 7.0]
 
