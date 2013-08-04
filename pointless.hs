806b1e894dde790427faf40ba265cea4265c8ec3
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:25:14 +1000

    Inline summer

--- pointless.hs
+++ pointless.hs
@@ -1,9 +1,8 @@
 import Control.Arrow
 
-summer = (+)
 lengther = (+) . const 1
 dup f a = f a a
-bit = foldr (uncurry (***) . (summer *** lengther)) (0.0, 0.0)
+bit = foldr (uncurry (***) . ((+) *** lengther)) (0.0, 0.0)
 average = uncurry (/) . bit . dup zip
 
 xs = [2.0, 5.0, 7.0]
