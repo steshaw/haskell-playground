a415608467b736c6ae617cec9f5875a772ce2c08
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:24:07 +1000

    Less brackety

--- pointless.hs
+++ pointless.hs
@@ -5,7 +5,7 @@ lengther = (+) . const 1
 myapp = uncurry (***)
 dup f a = f a a
 bit = foldr (myapp . (summer *** lengther)) (0.0, 0.0)
-average = (uncurry (/) . bit . dup zip)
+average = uncurry (/) . bit . dup zip
 
 xs = [2.0, 5.0, 7.0]
 
