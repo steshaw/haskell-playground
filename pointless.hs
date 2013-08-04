1b88e24fc80ed136c5100f368428a48ef3c0dcff
Steven Shaw <steshaw@gmail.com>
2013-08-04 23:05:33 +1000

    Add type signatures

--- pointless.hs
+++ pointless.hs
@@ -1,8 +1,11 @@
 import Control.Arrow
 import Control.Monad
 
-average = uncurry (/) . foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0.0) . join zip
+average :: [Double] -> Double
+average = uncurry (/) . foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0) . join zip
 
+xs :: [Double]
 xs = [2.0, 5.0, 7.0]
 
+result :: Double
 result = average xs
