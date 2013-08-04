802d268c51cb8e5834d57efcde2fd512e88ac4e9
Steven Shaw <steshaw@gmail.com>
2013-08-04 22:25:41 +1000

    Inline lengther

--- pointless.hs
+++ pointless.hs
@@ -1,8 +1,7 @@
 import Control.Arrow
 
-lengther = (+) . const 1
 dup f a = f a a
-bit = foldr (uncurry (***) . ((+) *** lengther)) (0.0, 0.0)
+bit = foldr (uncurry (***) . ((+) *** (+) . const 1)) (0.0, 0.0)
 average = uncurry (/) . bit . dup zip
 
 xs = [2.0, 5.0, 7.0]
