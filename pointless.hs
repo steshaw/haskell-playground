7570f1f142720f60bd0970d69ff4e5ee112c6f81
Steven Shaw <steshaw@gmail.com>
2013-08-07 06:49:37 +1000

    Add type sig

--- pointless.hs
+++ pointless.hs
@@ -6,11 +6,12 @@ average = uncurry (/) . foldl (uncurry (***) . ((+) *** flip ((+) . const 1))) (
 
 averageBits :: [Double] -> (Double, Integer)
 averageBits xs = foldl f (0.0, 0) (map (id &&& (const 1)) xs)
-  where 
+  where
     f :: (Double, Integer) -> (Double, Integer) -> (Double, Integer)
     f (accSum, accLen) (d, i) = (accSum + d, accLen + i)
 
-average' xs = (uncurry (/) . second fromInteger) (averageBits xs)
+average' :: [Double] -> Double
+average' = (uncurry (/) . second fromInteger) . averageBits
 
 xs :: [Double]
 xs = [2.0, 5.0, 7.0]
