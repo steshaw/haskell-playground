3fdedfaf697705c633c8ee9cba35b05e88001e31
Steven Shaw <steshaw@gmail.com>
2013-08-07 06:42:11 +1000

    Pointy non-foldM

--- pointless.hs
+++ pointless.hs
@@ -4,6 +4,14 @@ import Control.Monad
 average :: [Double] -> Double
 average = uncurry (/) . foldl (uncurry (***) . ((+) *** flip ((+) . const 1))) (0.0, 0) . join zip
 
+averageBits :: [Double] -> (Double, Integer)
+averageBits xs = foldl f (0.0, 0) (map (id &&& (const 1)) xs)
+  where 
+    f :: (Double, Integer) -> (Double, Integer) -> (Double, Integer)
+    f (accSum, accLen) (d, i) = (accSum + d, accLen + i)
+
+average' xs = (uncurry (/) . second fromInteger) (averageBits xs)
+
 xs :: [Double]
 xs = [2.0, 5.0, 7.0]
 
