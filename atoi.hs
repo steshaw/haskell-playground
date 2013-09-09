ee9d29a81ce9ef03debcb618f0362fe8dc08beaf
Steven Shaw <steven@steshaw.org>
2013-09-10 02:50:40 +1000

    Eking out a shorter version

--- atoi.hs
+++ atoi.hs
@@ -1,13 +1,18 @@
-import Control.Monad (foldM)
+import Control.Monad (foldM, forM_)
+import Data.Char (isDigit, ord)
 
-atoi :: String -> Int
-atoi = foldl (\acc n -> fromEnum n - fromEnum '0' + acc * 10) 0
+atoi0 :: String -> Int
+atoi0 = foldl (\acc n -> fromEnum n - fromEnum '0' + acc * 10) 0
 
-examples = ["", "1", "12", "123", "a", "asdf"]
+examples = ["", "1", "12", "123", "1234", "Hi", "asdf"]
 
-r1 = map atoi examples
+r1 = map atoi0 examples
+p1 = r1 `forM_` (putStrLn.show)
 
-digitToInt digit = case digit of
+type Digit = Char
+
+digitToInt0 :: Digit -> Maybe Int
+digitToInt0 digit = case digit of
   '0' -> Just 0
   '1' -> Just 1
   '2' -> Just 2
@@ -20,15 +25,33 @@ digitToInt digit = case digit of
   '9' -> Just 9
   _   -> Nothing
 
-type Digit = Char
+digitToInt :: Digit -> Maybe Int
+digitToInt digit = if isDigit digit then Just (ord digit - ord '0') else Nothing
 
-atoi' :: String -> Maybe Int
-atoi' "" = Nothing
-atoi' s = foldM f 0 s
+atoi1 :: String -> Maybe Int
+atoi1 "" = Nothing
+atoi1 s = foldM f 0 s
   where
     f :: Int -> Digit -> Maybe Int
     f acc digit = do
       n <- digitToInt digit
       return $ n + acc * 10
 
-r2 = map atoi' examples
+r2 = map atoi1 examples
+p2 = r2 `forM_` (putStrLn.show)
+
+fromBool b a = if b then Just a else Nothing
+
+--
+-- Eking out a short version...
+--
+atoi "" = Nothing
+atoi s = foldM f 0 s
+  where
+    f acc digit = do
+      n <- digitToInt digit
+      return $ n + acc * 10
+    digitToInt digit = fromBool (isDigit digit) (ord digit - ord '0')
+
+r3 = map atoi examples
+p3 = r3 `forM_` (putStrLn.show)
