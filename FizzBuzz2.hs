f8aab2a8905e5fc279966b604ca9d140ccb74e62
Steven Shaw <steven@steshaw.org>
2013-12-27 13:05:36 +1000

    With Data.Monoid.mappend

--- FizzBuzz2.hs
+++ FizzBuzz2.hs
@@ -3,13 +3,15 @@
  -}
 module Main where
 
-every3 = cycle $ replicate 2 "" ++ ["Fizz"]
-every5 = cycle $ replicate 4 "" ++ ["Buzz"]
+import Data.Monoid (mappend)
 
-fizzBuzzes = zipWith (++) every3 every5
+every3 = cycle $ replicate 2 Nothing ++ [Just "Fizz"]
+every5 = cycle $ replicate 4 Nothing ++ [Just "Buzz"]
+
+fizzBuzzes = zipWith mappend every3 every5
 
 numberedFizzBuzzes = zipWith f [1..] fizzBuzzes
   where
-    f n fb = if null fb then show n else fb
+    f n fb = maybe (show n) id fb
 
 main = putStr (unlines (take 100 numberedFizzBuzzes))
