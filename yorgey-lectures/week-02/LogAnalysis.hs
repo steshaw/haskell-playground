93e5a5366e597d754c1976d4c5549d0694be0b7b
Steven Shaw <steven@steshaw.org>
2014-04-24 17:44:06 +1000

    Seems to work

--- yorgey-lectures/week-02/LogAnalysis.hs
+++ yorgey-lectures/week-02/LogAnalysis.hs
@@ -31,6 +31,44 @@ parseMessageType :: [String] -> Maybe (MessageType, [String])
 --parseMessageType _ -> Nothing
 parseMessageType = undefined
 
+extractTimestamp :: LogMessage -> TimeStamp
+extractTimestamp (LogMessage _ timestamp _) = timestamp
+extractTimestamp _ = undefined -- this will never ever happen :)
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree           = tree
+insert msg Leaf                   = Node Leaf msg Leaf
+insert msg (Node left nmsg right) = case compare tmsg tnmsg of
+                  								  	LT -> Node (insert msg left) nmsg right
+                  								  	_  -> Node left nmsg (insert msg right)
+                  								  where
+                    									tmsg = extractTimestamp msg
+                    									tnmsg = extractTimestamp nmsg
+
+build :: [LogMessage] -> MessageTree
+build ms = build0 ms Leaf
+  where
+     build0 [] t = t
+     build0 (m:ms') t = build0 ms' (insert m t)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder t = inOrder0 t []
+  where
+     inOrder0 Leaf ms = ms
+     inOrder0 (Node l m r) ms = inOrder0 l ms ++ [m] ++ inOrder0 r ms
+
+extractString :: LogMessage -> String
+extractString (LogMessage _ _ s) = s
+extractString _  = undefined
+
+severeError :: LogMessage -> Bool
+severeError (LogMessage (Error severity) _ _) | severity >= 50 = True
+severeError _                                                  = False
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map extractString . filter severeError . sort
+  where sort = inOrder . build
+
 goodInt :: String -> Bool
 goodInt s = case (reads s :: [(Int, String)]) of
   [(_, "")] -> True
@@ -42,8 +80,33 @@ toInt = read
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
-poke :: IO [LogMessage]
-poke = testParse parse 10 "error.log"
+type Filename = String
 
-poke' :: IO ()
-poke' = poke >>= mapM_ print
+poke :: Filename -> IO [LogMessage]
+poke filename = testParse parse 100 filename
+
+pokeIt :: Filename -> IO ()
+pokeIt filename = do
+  msgs <- poke filename
+  print $ whatWentWrong msgs
+
+pokeError :: IO ()
+pokeError = pokeIt "error.log"
+
+pokeSample :: IO ()
+pokeSample = pokeIt "sample.log"
+
+test :: Filename -> IO [String]
+test filename = testWhatWentWrong parse whatWentWrong filename
+
+testError :: IO [String]
+testError = test "error.log"
+
+testSample :: IO [String]
+testSample = test "sample.log"
+
+testError' :: IO ()
+testError' = testError >>= mapM_ print
+
+testSample' :: IO ()
+testSample' = testError >>= mapM_ print
