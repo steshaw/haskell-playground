621d7ec34d4fc77628c6b37babdbb4b5daa6bc92
Steven Shaw <steven@steshaw.org>
2014-05-05 11:38:26 +1000

    Alternative oops using lens utility

--- oops.hs
+++ oops.hs
@@ -2,11 +2,14 @@ module Main where
 
 import qualified Prelude as P ((.))
 import Prelude hiding ((.))
+import Control.Lens
 
 data D = D {s::String}
 
 (∘) = (P..)
 (.) = flip ($); d = D "oops"; f = filter (< 's')
 
-a = length ∘ f ∘ s $ d
-b = d.s.f.length
+a  = length ∘ f ∘ s $ d
+b  = d.s.f.length
+c  = d&s&f&length -- alternative using a utility from Control.Lens. Thanks to Javran Uptoiso https://twitter.com/JavranC.
+c' = d & s & f & length -- a more readable alternative but still prefer the "OO" syntax.
