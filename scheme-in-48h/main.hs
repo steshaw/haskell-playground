91abdea64f07b882466ab442078f67c3d415adf8
Steven Shaw <steven@steshaw.org>
2014-12-31 02:25:04 +1000

    Some parsing

--- scheme-in-48h/main.hs
+++ scheme-in-48h/main.hs
@@ -1,8 +1,59 @@
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad (liftM)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+  deriving (Show)
+
+dq = '"'
+
+parseString :: Parser LispVal
+parseString = do
+  char dq
+  x <- many (noneOf [dq])
+  char dq
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr =
+      parseAtom
+  <|> parseString
+  <|> parseNumber
+
+symbol :: Parser Char
+symbol = oneOf "!#$%|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Ok: " ++ show val
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of [a,b] -> putStrLn $ "Sum is " ++ (show ((read a) + (read b)))
+  case args of
+    [expr] -> putStrLn $ readExpr expr
