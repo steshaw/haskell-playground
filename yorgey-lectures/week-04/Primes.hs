5fa36e105e09809a35f8bc89fc1fa663e4abaf3b
Steven Shaw <steven@steshaw.org>
2015-01-15 01:12:32 +1000

    Use Data.Set (\\) and additional implementation using just Data.List

--- yorgey-lectures/week-04/Primes.hs
+++ yorgey-lectures/week-04/Primes.hs
@@ -5,20 +5,33 @@ infixl 0 |>
 -}
 
 import qualified Data.Set as S
+import Data.List
 
 cartProd :: [a] -> [b] -> [(a, b)]
 cartProd xs ys = [(x, y) | x <- xs, y <- ys]
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram n = map twiddle . filter notInUnprimes $ nums
+sieveSundaram n = map twiddle . S.toList $ setNums S.\\ unprimes
+  where
+    nums :: [Integer]
+    nums = [1..n]
+    setNums :: S.Set Integer
+    setNums = S.fromList nums
+    twiddle :: Integer -> Integer
+    twiddle m = m * 2 + 1
+    unprimes :: S.Set Integer
+    unprimes = S.fromList . takeWhile (<= n) . map f . filter (uncurry (<=)) $ cartProd nums nums
+      where
+        f (i, j) = i + j + (2 * i * j)
+
+sieveSundaram1 :: Integer -> [Integer]
+sieveSundaram1 n = map twiddle $ nums \\ unprimes
   where
     nums :: [Integer]
     nums = [1..n]
     twiddle :: Integer -> Integer
     twiddle m = m * 2 + 1
-    notInUnprimes :: Integer -> Bool
-    notInUnprimes e = S.notMember e unprimes
-    unprimes :: S.Set Integer
-    unprimes = S.fromList . takeWhile (<= n) . map f . filter (uncurry (<=)) $ cartProd nums nums
+    unprimes :: [Integer]
+    unprimes = takeWhile (<= n) . map f . filter (uncurry (<=)) $ cartProd nums nums
       where
         f (i, j) = i + j + (2 * i * j)
