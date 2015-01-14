ab973d991d67014ba96dd3804feca20e48c43077
Steven Shaw <steven@steshaw.org>
2015-01-14 23:22:02 +1000

    Implement fun2 with iterate and takeWhile

--- yorgey-lectures/week-04/WholemealProgramming.hs
+++ yorgey-lectures/week-04/WholemealProgramming.hs
@@ -16,9 +16,13 @@ fun2 n | even n    = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
 fun2_ :: Integer -> Integer
-fun2_ = error "oops"
--- iterate and takeWhile ???
-
-
--- Exercise 2: Folding with trees
+fun2_ o
+  | o <= 0    = sum $ iterate f o -- loops indefinitely (to match fun2)
+  | otherwise = go o
+  where
+    go = sum . takeWhile (>0) . filter even . iterate f
+    f 1 = 0
+    f n
+      | even n    = n `div` 2
+      | otherwise = 3 * n + 1
 
