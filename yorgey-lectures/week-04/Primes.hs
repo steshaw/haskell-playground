7ac92d24bc08c73b95582f0ae271cbd6b9087209
Steven Shaw <steven@steshaw.org>
2015-01-15 00:56:10 +1000

    Use Data.Set notMember

--- yorgey-lectures/week-04/Primes.hs
+++ yorgey-lectures/week-04/Primes.hs
@@ -10,14 +10,14 @@ cartProd :: [a] -> [b] -> [(a, b)]
 cartProd xs ys = [(x, y) | x <- xs, y <- ys]
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram n = map twiddle . filter (not . inUnprimes) $ nums
+sieveSundaram n = map twiddle . filter notInUnprimes $ nums
   where
     nums :: [Integer]
     nums = [1..n]
     twiddle :: Integer -> Integer
     twiddle m = m * 2 + 1
-    inUnprimes :: Integer -> Bool
-    inUnprimes e = S.member e unprimes
+    notInUnprimes :: Integer -> Bool
+    notInUnprimes e = S.notMember e unprimes
     unprimes :: S.Set Integer
     unprimes = S.fromList . takeWhile (<= n) . map f . filter (uncurry (<=)) $ cartProd nums nums
       where
