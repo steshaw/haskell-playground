9fee1811d22d86a6c18e5979b480ddc38b1280e6
Steven Shaw <steven@steshaw.org>
2015-01-14 20:51:26 +1000

    Trouble with foldl

--- yorgey-lectures/week-04/MoreFolds.hs
+++ yorgey-lectures/week-04/MoreFolds.hs
@@ -8,22 +8,50 @@ xor = foldr f False
 map_ :: (a -> b) -> [a] -> [b]
 map_ f = foldr ((:) . f) []
 
-data Op
-  = Minus
-  | Plus
-  deriving (Show)
-
 data Exp
-  = Op Op Exp Exp
+  = Op Exp Exp
   | Lit Integer
-  deriving (Show)
+  deriving (Show, Eq)
 
-t1 = foldr (-) 1 [4, 2] == 4 - (2 - 1)
-t2 = foldl (-) 1 [4, 2] == (1 - 4) - 2
+r1 :: Integer
+r1 = 4 - (2 - 1)
+t1 :: Bool
+t1 = foldr (-) 1 [4, 2] == r1
+r2 :: Integer
+r2 = (1 - 4) - 2
+t2 :: Bool
+t2 = foldl (-) 1 [4, 2] == r2
 
-myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl f a bs = foldr fr a bs
-  where
-    fr :: a -> b -> b
-    fr = error "oops"
+foldl_ :: (a -> b -> a) -> a -> [b] -> a
+foldl_ _ a [] = a
+foldl_ f a (b:bs) = foldl_ f (f a b) bs
 
+foldLeft :: (a -> b -> a) -> a -> [b] -> a
+foldLeft f start bs = foldr (flip f) start bs
+
+fr :: Integer -> Exp -> Exp
+fr n e = Op (Lit n) e
+
+fl :: Exp -> Integer -> Exp
+fl = flip fr
+
+fr1 :: Exp
+fr1 = 4 `fr` (2 `fr` (Lit 1))
+
+fl1 :: Exp
+fl1 = ((Lit 1) `fl` 4) `fl` 2
+
+left :: Exp -> Integer -> Exp
+left e n = Op e (Lit n)
+
+want1 :: Exp
+want1 = (Lit 1 `Op` Lit 4) `Op` Lit 2
+
+want2 :: Exp
+want2 = (Lit 1 `left` 4) `left` 2
+
+want3 :: Exp
+want3 = foldl_   left (Lit 1) [4, 2]
+
+want4 :: Exp
+want4 = foldLeft left (Lit 1) [4, 2]
