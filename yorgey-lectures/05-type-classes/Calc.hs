746398865cc0ef27ed99679393240e86c17b9b86
Steven Shaw <steven@steshaw.org>
2015-01-15 23:12:06 +1000

    Expr type class

---
+++ yorgey-lectures/05-type-classes/Calc.hs
@@ -0,0 +1,56 @@
+import ExprT
+import Parser
+import Control.Applicative
+
+eval :: ExprT -> Integer
+eval (Lit n)   = n
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
+
+evalStr :: String -> Maybe Integer
+evalStr s = eval <$> parseExp Lit Add Mul s
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit n
+    | n <= 0     = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax $ max a b
+  mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit n = Mod7 $ n `mod` 7
+  add (Mod7 a) (Mod7 b) = lit $ a + b
+  mul (Mod7 a) (Mod7 b) = lit $ a * b
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
