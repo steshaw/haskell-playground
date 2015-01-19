af84b6b147b6b21ce11d4ea27e0d00396a4df922
Steven Shaw <steven@steshaw.org>
2015-01-19 12:15:24 +1000

    s-expression parser

--- yorgey-lectures/11-applicative2/SExpr.hs
+++ yorgey-lectures/11-applicative2/SExpr.hs
@@ -6,26 +6,27 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (satisfy isSpace)
 
 ident :: Parser String
-ident = undefined
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -44,3 +45,21 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+withSpaces :: Parser a -> Parser a
+withSpaces p = spaces *> p <* spaces
+
+--
+-- atom ::= int | ident
+--
+atom :: Parser Atom
+atom = N <$> posInt
+   <|> I <$> ident
+
+--
+-- S ::= atom | '(' S* ')'
+--
+sexpr :: Parser SExpr
+sexpr = withSpaces $
+      A <$> atom
+  <|> Comb <$> (char '(' *> (zeroOrMore sexpr) <* char ')')
