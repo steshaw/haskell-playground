0c842b1d04a1a91dd129f163f30474637fa1d123
Steven Shaw <steven@steshaw.org>
2015-01-25 15:58:38 +1000

    Implement battle

--- yorgey-lectures/12-monads/Risk.hs
+++ yorgey-lectures/12-monads/Risk.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Risk where
+--module Risk where
 
 import Control.Monad.Random
+import Control.Arrow (first)
+import Data.List (partition, sortBy)
 
 ------------------------------------------------------------
 -- Die values
@@ -10,8 +12,10 @@ import Control.Monad.Random
 newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
+{-
 first :: (a -> b) -> (a, c) -> (b, c)
 first f (a, c) = (f a, c)
+-}
 
 instance Random DieValue where
   random           = first DV . randomR (1,6)
@@ -25,4 +29,25 @@ die = getRandom
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield = Battlefield { bfAttackers :: Army, bfDefenders :: Army }
+  deriving (Show)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf@(Battlefield {bfAttackers = 0}) = return bf
+battle bf@(Battlefield {bfDefenders = 0}) = return bf
+battle (Battlefield attackers defenders) =
+  do
+    ad <- attackDie
+    dd <- defendDie
+    let results = zipWith (>) ad dd
+        losses = partition (==True) results
+        aLosses = length $ fst $ losses
+        dLosses = length $ snd $ losses
+      in battle $ Battlefield (attackers - aLosses) (defenders - dLosses)
+    where
+      numAttackers = min attackers 3
+      numDefenders = min defenders 2
+      dies = fmap revSort . sequence . flip replicate die
+      revSort = sortBy (flip compare)
+      attackDie = dies numAttackers
+      defendDie = dies numDefenders
