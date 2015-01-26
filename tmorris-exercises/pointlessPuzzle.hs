efa11f2420972d2349cdea5f46759a2806394c5b
Steven Shaw <steven@steshaw.org>
2015-01-26 19:24:23 +1000

    Using monoids in a pointless puzzle

--- tmorris-exercises/pointlessPuzzle.hs
+++ tmorris-exercises/pointlessPuzzle.hs
@@ -1,9 +1,19 @@
 --
--- See https://twitter.com/#!/dibblego/status/130796120107532289
 -- "Compute the average of an immutable single linked list (cons list) of integers. No variables."
+-- See https://twitter.com/dibblego/status/130796120107532289
 --
 
-import Control.Arrow
+import Control.Arrow ((&&&), (***))
+import Control.Category ((>>>))
 import Data.List (genericLength)
+import Data.Monoid (Sum(..), getSum)
+import Data.Foldable (foldMap)
 
-compute = sum &&& genericLength >>> uncurry (/)
+average :: Fractional n => [n] -> n
+average = sum &&& genericLength >>> uncurry (/)
+
+-- Using monoids.
+average' :: Fractional n => [n] -> n
+average' = foldMap (Sum &&& Sum . const 1) >>> getSum *** getSum >>> uncurry (/)
+
+eg1 = average' $ map fromIntegral [1..5]
