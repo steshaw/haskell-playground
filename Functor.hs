9bf49572b7b7f543e4c679bf72eadba303634bec
Steven Shaw <steven@steshaw.org>
2017-04-25 18:25:12 +1000

    Functor with extra parameters - wrong

--- Functor.hs
+++ Functor.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
 import Prelude hiding (Functor, map)
 
-class Functor f where
+-- Here's what goes wrong if you think that `a` and `b` should
+-- be parameters to `Functor`.
+class Functor a b f where
   map :: (a -> b) -> (f a -> f b)
 
 data List a
@@ -8,6 +14,13 @@ data List a
   | Cons a (List a)
   deriving (Show)
 
-instance Functor List where
+instance Functor a b List where
+  map :: (a -> b) -> (List a -> List b)
   map _ Nil = Nil
   map f (Cons a as) = Cons (f a) (map f as)
+
+list :: List Integer
+list = Cons 1 (Cons 2 (Cons 3 Nil))
+
+list1 :: List Integer
+list1 = map (+1) list
