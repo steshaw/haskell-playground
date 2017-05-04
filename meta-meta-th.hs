bed6ab5f321259d290bf406236de27fad6aa5f6b
Steven Shaw <steven@steshaw.org>
2017-05-04 11:04:10 +1000

    Meta meta TH example

---
+++ meta-meta-th.hs
@@ -0,0 +1,23 @@
+#!/usr/bin/env stack
+{-
+  stack script
+    --resolver lts-8.5
+    --package template-haskell
+    --
+    -Wall -fwarn-tabs -ddump-splices
+-}
+
+-- Example of "meta meta" TH.
+-- http://www.yesodweb.com/blog/2010/09/yo-dawg-template-haskell
+
+{-# LANGUAGE TemplateHaskell #-}
+import Language.Haskell.TH.Syntax
+
+main = $(return $(return $ InfixE
+    (Just $
+        (ConE $ mkName "VarE") `AppE`
+        ((VarE $ mkName "mkName") `AppE` (LitE $ StringL "putStrLn")))
+    (ConE $ mkName "AppE")
+    (Just $
+        (ConE $ mkName "LitE") `AppE`
+        ((ConE $ mkName "StringL") `AppE` (LitE $ StringL "Yo dawg")))))
