74748606aaf3dd15d1b0ecf0ca0c83fac69eae06
Steven Shaw <steven@steshaw.org>
2017-05-18 17:30:08 +1000

    Add routes to simple Wai app

--- warp.hs
+++ warp.hs
@@ -1,6 +1,7 @@
 #!/usr/bin/env stack
 {-
   stack script --resolver lts-8.8
+    --package bytestring
     --package http-types
     --package wai
     --package warp
@@ -8,7 +9,7 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Exception (bracket_)
+import Data.ByteString.Lazy
 import Network.HTTP.Types.Status
 import Network.Wai
 import Network.Wai.Handler.Warp (runEnv)
@@ -16,10 +17,18 @@ import Network.Wai.Handler.Warp (runEnv)
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 
 app :: Application
-app req respond = bracket_
-  (putStrLn "Allocating scarce resource")
-  (putStrLn "Cleaning up")
-  (respond $ responseLBS status200 [] "Hello World")
+app req respond =
+  respond $ case (requestMethod req, pathInfo req) of
+    ("GET", []) ->
+      html status200 "<h1>Welcome</h1>"
+    ("GET", ["login"]) ->
+      html status200 "<h1>Please login</h1>"
+    _ -> 
+      html status404 "<h1>Not found</h1>"
+
+html :: Status -> ByteString -> Response
+html status body =
+  responseLBS status [("Content-Type", "text/html")] body
 
 main :: IO ()
 main = runEnv 3000 app
