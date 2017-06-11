1aaea1a61d7b758500a282ed7349621c85fddb06
Steven Shaw <steven@steshaw.org>
2017-06-11 23:05:33 +1000

    Simplified version

--- non-closeable-channels/multi-worker-adapted.hs
+++ non-closeable-channels/multi-worker-adapted.hs
@@ -3,8 +3,6 @@
   stack --resolver lts-8.17 script
     --package async
     --package bytestring
-    --package either
-    --package mtl
     --package text
     --
     -Wall -fwarn-tabs
@@ -13,37 +11,30 @@
 
 import Control.Concurrent
 import Control.Concurrent.Async
-import Control.Monad (forever)
-import Control.Monad.Trans
-import Control.Monad.Trans.Either
 import Data.Text (Text, pack)
 import Data.Text.Encoding (encodeUtf8)
-import Data.Void
 
 import qualified Data.ByteString.Char8 as S8
 
 say :: Text -> IO ()
 say = S8.putStrLn . encodeUtf8
 
-exit :: e -> EitherT e IO a
-exit = left
-
-worker :: Chan (Maybe Int) -> Int -> IO (Either () Void)
-worker chan num =
-  runEitherT $
-  forever $ do
-    mi <- lift $ readChan chan
-    case mi of
-      Nothing -> do
-        exit ()
-      Just i -> do
-        lift $
+worker :: Chan (Maybe Int) -> Int -> IO ()
+worker chan num = loop
+  where
+    loop = do
+      mi <- readChan chan
+      case mi of
+        Nothing -> do
+          pure ()
+        Just i -> do
           say $ pack $ concat ["Worker #", show num, " received value ", show i]
+          loop
 
 main :: IO ()
 main = do
   chan <- newChan
   let workers = [1 .. 5]
-  let nothings = replicate (length workers) Nothing
+  let nothings = map (const Nothing) workers
   mapConcurrently_ (worker chan) workers `concurrently_`
-    writeList2Chan chan (fmap Just [1 .. 10] ++ nothings)
+    mapM_ (writeChan chan) (fmap Just [1 .. 10] ++ nothings)
