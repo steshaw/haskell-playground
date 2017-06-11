cb19c1ad0b649a62467f6e11cbdacca62270243c
Steven Shaw <steven@steshaw.org>
2017-06-11 18:22:13 +1000

    Formating with hindent

--- non-closeable-channels/multi-worker.hs
+++ non-closeable-channels/multi-worker.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
-{- 
+{-
   stack --resolver lts-8.17 script
     --package async
     --package bytestring
@@ -7,32 +7,27 @@
     --
     -Wall -fwarn-tabs
 -}
-
 -- {-# LANGUAGE OverloadedStrings #-}
-
 import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Monad (forever)
 import Data.Text (Text, pack)
 import Data.Text.Encoding (encodeUtf8)
+
 import qualified Data.ByteString.Char8 as S8
 
 say :: Text -> IO ()
 say = S8.putStrLn . encodeUtf8
 
 worker :: Chan Int -> Int -> IO ()
-worker chan num = forever $ do
+worker chan num =
+  forever $ do
     i <- readChan chan
-    say $ pack $ concat
-        [ "Worker #"
-        , show num
-        , " received value "
-        , show i
-        ]
+    say $ pack $ concat ["Worker #", show num, " received value ", show i]
 
 main :: IO ()
 main = do
   chan <- newChan
-  mapConcurrently_ (worker chan) [1..5] `concurrently_`
-       mapM_ (writeChan chan) [1..10]
-  return ()
+  mapConcurrently_ (worker chan) [1 .. 5] `concurrently_`
+    mapM_ (writeChan chan) [1 .. 10]
+  pure ()
