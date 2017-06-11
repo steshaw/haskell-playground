b5ab14a428494c6f300a635ea0c7ba9be56ad7b9
Steven Shaw <steven@steshaw.org>
2017-06-11 22:58:54 +1000

    runEitherT method

--- non-closeable-channels/multi-worker-adapted.hs
+++ non-closeable-channels/multi-worker-adapted.hs
@@ -14,32 +14,36 @@
 import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Monad (forever)
-import Control.Monad.Trans.Either (left, runEitherT)
+import Control.Monad.Trans
+import Control.Monad.Trans.Either
 import Data.Text (Text, pack)
 import Data.Text.Encoding (encodeUtf8)
+import Data.Void
 
 import qualified Data.ByteString.Char8 as S8
 
 say :: Text -> IO ()
 say = S8.putStrLn . encodeUtf8
 
--- exit = left
+exit :: e -> EitherT e IO a
+exit = left
 
-worker :: Chan (Maybe Int) -> Int -> IO ()
-worker chan num = loop
-  where
-    loop = do
-      mi <- readChan chan
-      case mi of
-        Nothing -> do
-          pure () -- exit!
-        Just i -> do
+worker :: Chan (Maybe Int) -> Int -> IO (Either () Void)
+worker chan num =
+  runEitherT $
+  forever $ do
+    mi <- lift $ readChan chan
+    case mi of
+      Nothing -> do
+        exit ()
+      Just i -> do
+        lift $
           say $ pack $ concat ["Worker #", show num, " received value ", show i]
-          loop -- XXX: What recursion pattern do we have here?
 
 main :: IO ()
 main = do
   chan <- newChan
-  let workers = [1..5]
+  let workers = [1 .. 5]
+  let nothings = replicate (length workers) Nothing
   mapConcurrently_ (worker chan) workers `concurrently_`
-    writeList2Chan chan (fmap Just [1 .. 10] ++ replicate (length workers) Nothing)
+    writeList2Chan chan (fmap Just [1 .. 10] ++ nothings)
