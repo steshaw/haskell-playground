ad9f224f7e29c05f1d17640ee2dc2c77e7580a07
Steven Shaw <steven@steshaw.org>
2017-06-11 20:30:07 +1000

    hindent

--- non-closeable-channels/closable-channels-stm.hs
+++ non-closeable-channels/closable-channels-stm.hs
@@ -8,20 +8,30 @@
     --
     -Wall -fwarn-tabs
 -}
-
+-- {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 import Control.Applicative ((<|>))
 import Control.Concurrent.Async
 import Control.Concurrent.STM
+import qualified Data.ByteString.Char8 as S8
 import Data.Text (Text, pack)
 import Data.Text.Encoding (encodeUtf8)
-import qualified Data.ByteString.Char8 as S8
 
 say :: Text -> IO ()
 say = S8.putStrLn . encodeUtf8
 
-data TCChan a = TCChan (TChan a) (TVar Bool)
+data TCChan a = TCChan
+  { channel :: (TChan a)
+  , isClosed :: (TVar Bool)
+  } deriving (Show)
+
+instance Show (TChan a) where
+  show _ = "TChan" :: String
+
+instance Show (TVar a) where
+  show _ = "TVar" :: String
 
 newTCChan :: IO (TCChan a)
 newTCChan = atomically $ TCChan <$> newTChan <*> newTVar False
@@ -30,41 +40,38 @@ closeTCChan :: TCChan a -> IO ()
 closeTCChan (TCChan _ var) = atomically $ writeTVar var True
 
 writeTCChan :: TCChan a -> a -> IO ()
-writeTCChan (TCChan chan var) val = atomically $ do
+writeTCChan (TCChan chan var) val =
+  atomically $ do
     closed <- readTVar var
     if closed
-        -- Could use nicer exception types, or return a Bool to
-        -- indicate if writing failed
-        then error "Wrote to a closed TCChan"
-        else writeTChan chan val
+      -- Could use nicer exception types, or return a Bool to
+      -- indicate if writing failed.
+      then error "Wrote to a closed TCChan"
+      else writeTChan chan val
 
 readTCChan :: TCChan a -> IO (Maybe a)
-readTCChan (TCChan chan var) = atomically $
-    (Just <$> readTChan chan) <|> (do
-        closed <- readTVar var
-        check closed
-        return Nothing)
+readTCChan (TCChan chan var) =
+  atomically $
+  (Just <$> readTChan chan) <|>
+  (do closed <- readTVar var
+      check closed
+      return Nothing)
 
 worker :: TCChan Int -> Int -> IO ()
-worker chan num =
-    loop
+worker chan num = loop
   where
     loop = do
-        mi <- readTCChan chan
-        case mi of
-            Nothing -> return ()
-            Just i -> do
-                say $ pack $ concat
-                    [ "Worker #"
-                    , show num
-                    , " received value "
-                    , show i
-                    ]
-                loop
+      mi <- readTCChan chan
+      case mi of
+        Nothing -> return ()
+        Just i -> do
+          say $ pack $ concat ["Worker #", show num, " received value ", show i]
+          loop
 
 main :: IO ()
 main = do
-    chan <- newTCChan
-    mapConcurrently (worker chan) [1..5] `concurrently_` do
-        mapM_ (writeTCChan chan) [1..10]
-        closeTCChan chan
+  chan <- newTCChan
+  putStrLn $ "channel = " ++ show chan
+  mapConcurrently (worker chan) [1 .. 5] `concurrently_` do
+    mapM_ (writeTCChan chan) [1 .. 10]
+    closeTCChan chan
