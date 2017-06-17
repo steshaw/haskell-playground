c25c062588da22b42fd6023ca7930dcccfe84bf1
Steven Shaw <steven@steshaw.org>
2017-06-17 16:29:04 +1000

    Output filepath as string

--- nix-derivation-parser/query-outputs.hs
+++ nix-derivation-parser/query-outputs.hs
@@ -3,11 +3,14 @@
 import Parser
 
 import Prelude hiding (FilePath)
+import Data.Monoid ((<>))
 
 import qualified Data.Attoparsec.ByteString.Lazy as P
 import qualified Data.ByteString.Lazy.Char8 as SIO
 import qualified Options.Generic
 import qualified Prelude
+import qualified Filesystem.Path.CurrentOS as FS
+import qualified Data.Text.IO as TIO
 
 process :: Prelude.FilePath -> IO ()
 process fileName = do
@@ -15,7 +18,12 @@ process fileName = do
   case P.eitherResult (P.parse parseDerivation text) of
     Left msg -> fail ("Parse failed: " ++ msg)
     Right derivation -> do
-      let printOutput output = print (path output)
+      let printOutput output =
+           let r = FS.toText (path output)
+               t = case r of
+                     Left brokenFilePath -> "Broken: " <> brokenFilePath
+                     Right filePath_ -> filePath_
+           in TIO.putStrLn t
       mapM_ printOutput (outputs derivation)
 
 main :: IO ()
