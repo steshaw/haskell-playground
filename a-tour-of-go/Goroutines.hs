e68538695e87a167ac5f6c0a6c4f9e0812cf4b66
Steven Shaw <steven@steshaw.org>
2017-12-11 20:57:48 +1000

    A Tour of Go in Haskell runnable with `stack script`.

---
+++ a-tour-of-go/Goroutines.hs
@@ -0,0 +1,19 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-9.17
+-}
+module A_Tour_of_Go.Concurrency.Goroutines where
+
+import Control.Concurrent.Async (async)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forM_)
+
+say :: String -> IO ()
+say s = forM_ [0..4] $ \i -> do
+  threadDelay $ 100 * 10^3
+  putStrLn s
+
+main :: IO ()
+main = do
+  async $ say "world"
+  say "hello"
