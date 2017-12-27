6db4758eba6cd406bdacb00a290f4ecb14e5af04
Steven Shaw <steven@steshaw.org>
2017-12-27 16:23:34 +1000

    Filter mimeTypes to the ones we want

---
+++ someMimeTypes.hs
@@ -0,0 +1,32 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-10.1
+    --package containers
+    --package hscolour
+    --package mime-types
+    --package pretty-show
+    --package text
+-}
+{-# language OverloadedStrings #-}
+
+import Data.Map.Strict
+import Network.Mime
+
+import Text.Show.Pretty (ppShow)
+import System.IO as IO
+import Language.Haskell.HsColour
+import Language.Haskell.HsColour.Colourise
+
+colorPrint thing = do
+  colourPrefs <- readColourPrefs
+  -- HT http://teh.id.au/posts/2017/02/13/interactive-print/index.html
+  IO.putStrLn . hscolour TTY colourPrefs False False "" False . ppShow $ thing
+
+mimeTypes = ["doc", "docx", "pdf", "rtf", "txt"]
+
+main = do
+  let mimeMap = filterWithKey (\k _ -> k `elem` mimeTypes) defaultMimeMap
+  colourPrefs <- readColourPrefs
+  colorPrint ("colourPrefs", colourPrefs)
+  colorPrint ("defaultColourPrefs", defaultColourPrefs)
+  colorPrint $ toList mimeMap
