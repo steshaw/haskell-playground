d23cfc4256adf2dcd38653838c649f60eec8139f
Steven Shaw <steven@steshaw.org>
2017-12-27 14:06:32 +1000

    A colourPrefs.hs

---
+++ colourPrefs.hs
@@ -0,0 +1,17 @@
+ColourPrefs
+  { keyword = [ Foreground Green , Underscore ]
+  , keyglyph = [ Foreground Red ]
+  , layout = [ Foreground Cyan ]
+  , comment = [ Foreground Blue , Italic ]
+  , conid = [ Normal ]
+  , varid = [ Normal ]
+  , conop = [ Foreground Red , Bold ]
+  , varop = [ Foreground Cyan ]
+  , string = [ Foreground Magenta ]
+  , char = [ Foreground Magenta ]
+  , number = [ Foreground Magenta ]
+  , cpp = [ Foreground Magenta , Dim ]
+  , selection = [ Bold , Foreground Magenta ]
+  , variantselection = [ Dim , Foreground Red , Underscore ]
+  , definition = [ Foreground Blue ]
+  }
