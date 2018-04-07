31e165ab7fcfc89578905da5713a1b598101c4a5
Steven Shaw <steven@steshaw.org>
2018-04-07 17:11:51 +1000

    Explicit imports

--- destroyallsoftwareScreencasts.hs
+++ destroyallsoftwareScreencasts.hs
@@ -36,21 +36,23 @@ $ wget -ci destroyallsoftwareScreencasts.txt # download screencasts
 import Control.Concurrent (rtsSupportsBoundThreads)
 import Control.Concurrent.Async (mapConcurrently)
 import Control.Lens hiding (element)
-import Control.Monad
-import Data.Semigroup
-import Text.XML
+import Control.Monad (forM_)
+import Data.Semigroup ((<>))
+import Text.XML (def, parseLBS_)
 import Text.XML.Cursor
+  (content, element, fromDocument, ($//), (&/))
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
-import Network.Wreq
-import System.IO (stderr)
 
 import GHC.Conc
+  (numCapabilities, getNumCapabilities, getNumProcessors, numSparks)
 import GHC.Exts (fromList)
 
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Network.Wreq as Wreq
+import qualified System.IO as SIO
 
 {-
 getScript :: Document -> [Text]
@@ -67,22 +69,22 @@ main = do
   gnp <- getNumProcessors
   ns <- numSparks
   mapM_
-    (TIO.hPutStrLn stderr . tshow)
-    [ ("numProcessors" :: Text, show nc)
-    , ("getNumCapabilities" :: Text, show gnc)
-    , ("getNumProcessors" :: Text, show gnp)
-    , ("numSparks" :: Text, show ns)
-    , ("rtsSupportsBoundThreads" :: Text, show rtsSupportsBoundThreads)
+    (TIO.hPutStrLn SIO.stderr . tshow)
+    [ ("numProcessors" :: Text, tshow nc)
+    , ("getNumCapabilities" :: Text, tshow gnc)
+    , ("getNumProcessors" :: Text, tshow gnp)
+    , ("numSparks" :: Text, tshow ns)
+    , ("rtsSupportsBoundThreads" :: Text, tshow rtsSupportsBoundThreads)
     ]
 
   doc <- do
-    r <- get "https://www.destroyallsoftware.com/screencasts/feed"
-    pure $ r ^. responseBody & Text.XML.parseLBS_ def
+    r <- Wreq.get "https://www.destroyallsoftware.com/screencasts/feed"
+    pure $ r ^. Wreq.responseBody & parseLBS_ def
   let cursor = fromDocument doc
   let links = cursor $// element "item" &/ element "link" &/ content
   (bodies :: [(Text, Text)]) <- flip mapConcurrently links $ \link -> do
-    r <- get (T.unpack link)
-    pure (link, r ^. responseBody & BSL.toStrict & decodeUtf8)
+    r <- Wreq.get (T.unpack link)
+    pure (link, r ^. Wreq.responseBody & BSL.toStrict & decodeUtf8)
   forM_ bodies $ \(_link, body) -> do
     let isMovie line = T.isInfixOf "source.src" line && T.isInfixOf ".mp4" line
     let movies = filter isMovie (T.lines body)
