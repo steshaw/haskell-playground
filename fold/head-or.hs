136c20f170ad66b1239dbbb1420695e74d216dfe
Steven Shaw <steven@steshaw.org>
2019-06-26 07:56:09 +1000

    ♻️ Eta reduction

--- fold/head-or.hs
+++ fold/head-or.hs
@@ -1,9 +1,11 @@
+module M where
+
 -- foldr  : (a -> b -> b) -> b -> [a] -> b
 -- foldr1 : (a -> a -> a) -> [a] -> a
 
 headOr :: Foldable t => a -> t a -> a
 headOr b = flip foldr b $
-  \a -> const a
+  const
 
 eg1 :: Int
 eg1 = headOr 0 [1 .. 3]
