553261a3e726fc31906a7d31d81c35b7ebab85a6
Steven Shaw <steven@steshaw.org>
2021-06-29 09:39:53 +1000

    unRLE

--- Runs.hs
+++ Runs.hs
@@ -16,6 +16,7 @@ import Prelude hiding (head)
 
 import Control.Arrow ((&&&))
 import Control.Monad (forM_, when)
+import Data.Semigroup (stimes)
 import GHC.Stack
 import qualified Data.List as L
 import qualified Data.List.NonEmpty as NEL
@@ -72,14 +73,29 @@ shouldEq a b =
     putStrLn $ "Actual  : " <> show b
     putStrLn (prettyCallStack callStack)
 
+s1 :: String
+s1 = "aaaabbbacc"
+rle1 :: [(Char, Integer)]
+rle1 = [('a', 4), ('b', 3), ('a', 1), ('c', 2)]
+s2 :: String
+s2 = "aaaabbbcca"
+rle2 :: [(Char, Integer)]
+rle2 = [('a', 4), ('b', 3), ('c', 2), ('a', 1)]
+
 testEg1 :: HasCallStack => (String -> [(Char, Integer)]) -> IO ()
-testEg1 f = f "aaaabbbacc" `shouldEq` [('a', 4), ('b', 3), ('a', 1), ('c', 2)]
+testEg1 f = f s1 `shouldEq` rle1
 
 testEg2 :: HasCallStack => (String -> [(Char, Integer)]) -> IO ()
-testEg2 f = f "aaaabbbcca" `shouldEq` [('a', 4), ('b', 3), ('c', 2), ('a', 1)]
+testEg2 f = f s2 `shouldEq` rle2
+
+unRLE :: [(x, Integer)] -> [x]
+unRLE = concatMap (\(c, n) -> stimes n [c])
 
 main :: IO ()
 main = do
+  unRLE rle1 `shouldEq` s1
+  unRLE rle2 `shouldEq` s2
+
   let runss = [runs0, runs1, runs2, runs2a, runs2b, runs3]
   let tests = [testEg1, testEg2]
 
