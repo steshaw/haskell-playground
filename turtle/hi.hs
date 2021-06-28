0996dc2cd4437eec4c16d22ee1912114721111c7
Steven Shaw <steven@steshaw.org>
2021-06-29 09:48:58 +1000

    Bump LTS because we don't want to download 100Mb to run a little script

--- turtle/hi.hs
+++ turtle/hi.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-8.5 --install-ghc runghc --package turtle
+-- stack --resolver lts-18.0 --install-ghc runghc --package turtle
 
 {-# LANGUAGE OverloadedStrings #-}
 
