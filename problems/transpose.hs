2e6394147c4341959ee4d82563035581d8c1c11f
Steven Shaw <steven@steshaw.org>
2021-07-03 14:06:55 +1000

    transpose

---
+++ problems/transpose.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE BlockArguments #-}
+
+import Prelude
+import Control.Monad (when)
+
+input :: [String]
+input = [
+  "aA",
+  "bB",
+  "cC"
+  ]
+
+expected :: [String]
+expected = [
+  "abc",
+  "ABC"
+  ]
+
+-- transpose?
+mystery :: [String] -> [String]
+mystery xs =
+  let shortest = minimum (map length xs) in
+  map (\i -> map (!! i) xs) [0 .. shortest - 1]
+
+main = do
+  let actual = mystery input
+  if actual /= expected then do
+    putStrLn $ "Expected: " <> show expected
+    putStrLn $ "Actual  : " <> show actual
+  else
+    putStrLn "ok"
