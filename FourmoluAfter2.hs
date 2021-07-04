40d1c7bfc058dadc8a979f6c73812a1a0b3ea902
Steven Shaw <steven@steshaw.org>
2021-07-05 07:14:59 +1000

    Try Fourmolu diff-friendly exports

---
+++ FourmoluAfter2.hs
@@ -0,0 +1,12 @@
+module Ormolu (
+  ormolu,
+  ormoluFile,
+  ormoluStdin,
+  -- ...
+  withPrettyOrmoluExceptions,
+) where
+
+ormolu = 'a'
+ormoluFile = ormolu
+ormoluStdin = ormolu
+withPrettyOrmoluExceptions = ormolu
---
+++ FourmoluBefore2.hs
@@ -0,0 +1,13 @@
+module Ormolu
+  ( ormolu,
+    ormoluFile,
+    ormoluStdin,
+    -- ...
+    withPrettyOrmoluExceptions,
+  )
+where
+
+ormolu = 'a'
+ormoluFile = ormolu
+ormoluStdin = ormolu
+withPrettyOrmoluExceptions = ormolu
